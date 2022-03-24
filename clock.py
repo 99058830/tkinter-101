9cd4c1a80ad9116e85a89dfef243d1cb9c7a63fa
99058830 <89842360+99058830@users.noreply.github.com>
2022-03-24 14:16:37 +0100

    comments

--- clock.py
+++ clock.py
@@ -6,12 +6,12 @@ root.title('Klokje')
  
 def time():
     string = strftime('%H:%M:%S %p')
-    chopper = string[:-2]
+    chopper = string[:-2] # removes chars AM or PM
     label.config(text = chopper)
-    label.after(1000, time)
+    label.after(1000, time) # callback na 1000
  
-label = Label(root, font = ('Helvetica', 25), bg = 'yellow', fg = 'red')
-label.pack(anchor = 'center')
+label = Label(root, font = ('Helvetica', 25), bg = 'yellow', fg = 'red') # style
+label.pack(anchor = 'center') # center label
 time()
  
 mainloop()
